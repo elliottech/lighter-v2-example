916e0a78578daaf34d27aa79ff3e7afe553ae98c
Alex Velea <dev@velea.cc>
2023-09-15 17:27:49 +0300

    setup repository #2
    - add project config files
    - add tasks & tests for to cover testnet ERC20

---
+++ contracts/interface/IERC20Metadata.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// import interface as side-effect, to generate types
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv'
+import {HardhatUserConfig} from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import './tasks'
+
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.18',
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://rpc.goerli.arbitrum.gateway.fm/`,
+      },
+    },
+    testnet: {
+      url: `https://rpc.goerli.arbitrum.gateway.fm/`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    mainnet: {
+      url: `https://arb1.arbitrum.io/rpc`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+}
+
+export default config
