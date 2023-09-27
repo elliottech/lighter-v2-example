a442a4bc14041481c1191c2279e3989e43e227fc
kanth <defi.kanth@gmail.com>
2023-09-28 00:21:58 +0530

    feat: add new supported networksfor lighter-v2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,18 @@ const config: HardhatUserConfig = {
       url: process.env.ARBITRUM_MAINNET_URL ? process.env.ARBITRUM_MAINNET_URL : `https://arb1.arbitrum.io/rpc`,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
+    optgoerli: {
+      url: process.env.OPTIMISM_GOERLI_URL ? process.env.OPTIMISM_GOERLI_URL : `https://optimism-goerli.publicnode.com`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    bsctestnet: {
+      url: process.env.BSC_TESTNET_URL ? process.env.BSC_TESTNET_URL : `https://bsc-testnet.publicnode.com`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI_URL ? process.env.POLYGON_MUMBAI_URL : `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
   },
 }
 
